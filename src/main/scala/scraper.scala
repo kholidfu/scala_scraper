efab3c7c3869c93a767438c3fc29cb5f20fc67c5
Fabrice Sznajderman <Fabszn@gmail.com>
2014-06-02 21:40:59 +0200

    Update scraper.scala
    Remove type

--- src/main/scala/scraper.scala
+++ src/main/scala/scraper.scala
@@ -6,16 +6,16 @@ import scala.collection.JavaConverters._
 object scraper {
   def main(args: Array[String]) {
     // set url
-    val url: String = "http://allbookez.com/"
+    val url = "http://allbookez.com/"
     // get the html Document
-    val doc: Document = Jsoup.connect(url).get()
+    val doc = Jsoup.connect(url).get()
     // parsing element
     val recents: Elements = doc.select(".moduleTitle")
     val nowLooking = recents.last()
     val nextSibling = nowLooking.nextElementSibling()
     val links = nextSibling.select("a")
     // convert java array to scala array and map it!
-    val keywords = links.asScala.map(link => link.text())
-    keywords.foreach(println)
+    links.asScala.map(link => link.text()).foreach(println)
+    
   }
 }
